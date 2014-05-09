63c271c4cc98eadf524ca7815a978ec071c82cfc
Seyfeddin Bassarac <seyfoyun@gmail.com>
2014-05-09 20:03:21 +0300

    Initial Commit

---
+++ popExample/PEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PEAppDelegate.h
+//  popExample
+//
+//  Created by Seyfeddin Bassarac on 09/05/14.
+//  Copyright (c) 2014 ThreadCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ popExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  popExample
+//
+//  Created by Seyfeddin Bassarac on 09/05/14.
+//  Copyright (c) 2014 ThreadCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PEAppDelegate class]));
+    }
+}
